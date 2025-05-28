5dbae0f48a3d4c1757d6e6c77752ec699486b0a3
sm4rtdev <elonscottdev@gmail.com>
2025-05-28 13:08:44 -0500

    clean unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,4 @@
 // src/app/layout.tsx
-// import "./globals.css";
-// import type { Metadata } from "next";
-
-// export const metadata: Metadata = {
-//   title: "MatrixFrog",
-//   description:
-//     "A choice between two worlds. Human or Frog? Reality or Simulation?",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         {children}
-//         </body>
-//     </html>
-//   );
-// }
-
-// src/app/layout.tsx
 import "./globals.css";
 import type { Metadata } from "next";
 import WalletProvider from "../app/providers/WalletProvider";
