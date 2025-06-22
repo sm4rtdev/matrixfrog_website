ca1a423ead51699a185a17dd52cf3a4216cffc6c
OluRemiFour <lekanayoola04@gmail.com>
2025-06-22 15:41:04 -0700

    fix upadates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -314,21 +314,23 @@ export default function Home() {
                   {/* Smaller and narrower banner format with fixed height for terminal */}
                   <div className="w-full flex justify-center fade-in fade-in-delay-1">
                     <div
-                      className={`terminal-container bg-black bg-opacity-50 border border-matrix-green rounded-md p-4 scale-in-center ${pillFlashEffect !== "none"
-                        ? "terminal-flash terminal-glitch"
-                        : ""
-                        }`}
+                      className={`terminal-container bg-black bg-opacity-50 border border-matrix-green rounded-md p-4 scale-in-center ${
+                        pillFlashEffect !== "none"
+                          ? "terminal-flash terminal-glitch"
+                          : ""
+                      }`}
                     >
                       {/* MatrixFrog Zitat (wenn vorhanden) oder Standard-Text */}
                       {matrixQuote ? (
                         <div className="text-matrix-green p-2 overflow-hidden">
                           <p
-                            className={`text-xl leading-relaxed ${quoteTransition === "fadeOut"
-                              ? "text-fade-out"
-                              : quoteTransition === "typing"
+                            className={`text-xl leading-relaxed ${
+                              quoteTransition === "fadeOut"
+                                ? "text-fade-out"
+                                : quoteTransition === "typing"
                                 ? "text-fade-in"
                                 : ""
-                              }`}
+                            }`}
                           >
                             {displayedQuote}
                             {quoteTransition === "typing" && (
@@ -383,7 +385,8 @@ export default function Home() {
                     style={{ animationDelay: "0.3s" }}
                   >
                     <Image
-                      src="/matrixfrog.png"
+                      // src="/matrixfrog.png"
+                      src="/MFG.png"
                       alt="Human Hand vs Frog Hand"
                       width={400}
                       height={400}
@@ -405,7 +408,9 @@ export default function Home() {
                       style={{ transform: "translateY(10px)" }}
                     >
                       <div className="text-center mb-2">
-                        <span className="glow-text-red text-[20px]">CHOOSE RED</span>
+                        <span className="glow-text-red text-[20px]">
+                          CHOOSE RED
+                        </span>
                       </div>
                       <div
                         className="image-container cursor-pointer pill-hover"
@@ -417,8 +422,9 @@ export default function Home() {
                           alt="Red Pill"
                           width={110}
                           height={110}
-                          className={`pill-image ${pillFlashEffect === "red" ? "pill-flash-red" : ""
-                            }`}
+                          className={`pill-image ${
+                            pillFlashEffect === "red" ? "pill-flash-red" : ""
+                          }`}
                           style={{
                             filter:
                               "drop-shadow(0 0 10px rgba(255, 0, 0, 0.7))",
@@ -452,8 +458,9 @@ export default function Home() {
                           alt="Green Pill"
                           width={110}
                           height={110}
-                          className={`pill-image ${pillFlashEffect === "blue" ? "pill-flash-blue" : ""
-                            }`}
+                          className={`pill-image ${
+                            pillFlashEffect === "blue" ? "pill-flash-blue" : ""
+                          }`}
                           style={{
                             filter:
                               // "drop-shadow(0 0 10px rgba(0, 100, 255, 0.7))",
